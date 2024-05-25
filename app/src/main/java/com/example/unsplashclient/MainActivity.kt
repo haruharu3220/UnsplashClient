a59eda4a82312cc8500c912750bc32cc62aaac87
Hayato <hayato.314.nico@gmail.com>
2024-05-26 01:55:11 +0900

    テストWidget追加

--- app/src/main/java/com/example/unsplashclient/MainActivity.kt
+++ app/src/main/java/com/example/unsplashclient/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) {
+                    Text(text="これはテストです")
 
                 }
             }
